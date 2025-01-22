90239064858c9576b701bb11c1d396ffa37f58fa
bentorrrr <bentorrrrr@gmail.com>
2025-01-22 16:53:26 +0700

    Init myBuffer as Superclass

--- Week2/src/myBuffer.java
+++ Week2/src/myBuffer.java
@@ -1,7 +1,32 @@
 public abstract class myBuffer {
   final int items[];
 
+  public myBuffer() {
+    this(10);
+  }
+
   public myBuffer(int size) {
     items = new int[size];
+    for (int i = 0; i < 10; i++) {
+        items[i] = 0;
+    }
   }
+
+  public void showBuffer(){
+    System.out.print("Items in" + this.getClass().getName() + ": [ ");
+    for (int i = 0; i < items.length; i++) {
+        System.out.print(items[i] + " ");
+    }
+    System.out.print("]");
+    System.out.println("");
+  }
+
+  public double average() {
+    double sum = 0;
+    for (int i = 0; i < items.length; i++) {
+        sum += items[i];
+    }
+    return items.length == 0 ? 0 : sum / items.length;
+  }
+
 }
