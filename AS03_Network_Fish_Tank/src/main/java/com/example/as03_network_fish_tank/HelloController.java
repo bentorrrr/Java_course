0aa36e9837e343fc7fcb8c4d69a7e7ea0d6f9887
Patcharaphon Santhitikul <bentorrrrr@gmail.com>
2025-05-27 18:19:08 +0700

    Creating Asignment 3 File

---
+++ AS03_Network_Fish_Tank/src/main/java/com/example/as03_network_fish_tank/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.as03_network_fish_tank;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
