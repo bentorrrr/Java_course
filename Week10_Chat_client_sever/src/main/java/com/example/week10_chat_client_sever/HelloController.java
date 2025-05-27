d3281b35c67d26eb57c36697772d4ad08c033e31
Patcharaphon Santhitikul <bentorrrrr@gmail.com>
2025-05-27 18:18:59 +0700

    Creating Week 10 Project file

---
+++ Week10_Chat_client_sever/src/main/java/com/example/week10_chat_client_sever/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.week10_chat_client_sever;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
